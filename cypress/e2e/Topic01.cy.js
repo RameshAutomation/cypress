dc67bb4c1f922947b480e133c5e870c29906b01c
Ramesh <rameshr@ideyalabs.com>
2023-05-28 18:09:28 +0530

    commit2

--- cypress/e2e/Topic01.cy.js
+++ cypress/e2e/Topic01.cy.js
@@ -8,7 +8,7 @@ describe('Topic01_Suite',() => {
         
         cy.visit('https://www.icicibank.com')
         cy.get("#new-header > div > div:nth-child(1) > div > div > nav.left-side-menu.menu-level-1-child > ul").should("be.visible")
-      //  cy.wait(3000)
+      //cy.wait(3000)
        // cy.viewport(500,800)
        cy.viewport("iphone-x")
         cy.wait(3000)
